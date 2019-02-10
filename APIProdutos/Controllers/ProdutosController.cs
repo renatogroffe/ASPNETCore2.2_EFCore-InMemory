57bdd81044ef32c72801a73e8766afe422a2a9ce
Renato Groffe <renato.groffe@yahoo.com.br>
2019-02-10 17:45:35 -0200

    ASP.NET Core 2.2 + Entity Framework Core InMemory + CORS
    ASP.NET Core 2.2 + Entity Framework Core InMemory + CORS

---
+++ APIProdutos/Controllers/ProdutosController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using APIProdutos.Business;
+using APIProdutos.Models;
+
+namespace APIProdutos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private ProdutoService _service;
+
+        public ProdutosController(ProdutoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<Produto> Get()
+        {
+            return _service.ListarTodos();
+        }
+
+        [HttpGet("{codigoBarras}")]
+        public ActionResult<Produto> Get(string codigoBarras)
+        {
+            var produto = _service.Obter(codigoBarras);
+            if (produto != null)
+                return produto;
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public Resultado Post(Produto produto)
+        {
+            return _service.Incluir(produto);
+        }
+
+        [HttpPut]
+        public Resultado Put(Produto produto)
+        {
+            return _service.Atualizar(produto);
+        }
+
+        [HttpDelete("{codigoBarras}")]
+        public Resultado Delete(string codigoBarras)
+        {
+            return _service.Excluir(codigoBarras);
+        }
+    }
+}
---
+++ APIProdutos/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using APIProdutos.Data;
+using APIProdutos.Business;
+
+namespace APIProdutos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("InMemoryDatabase"));
+            services.AddScoped<ProdutoService>();
+
+            services.AddCors();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+           app.UseCors(builder => builder.AllowAnyMethod()
+                                         .AllowAnyOrigin()
+                                         .AllowAnyHeader()
+                                         .AllowCredentials());
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
